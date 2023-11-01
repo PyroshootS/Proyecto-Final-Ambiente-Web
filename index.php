72c2feaf93efa76b68747dcc42922561424c9644
Pyro <oreinavallecillo@gmail.com>
2023-10-31 19:53:28 -0600

    index.php para avance clase 08

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!Doctype html>
+<html>
+<head>
+    <title>Avance Semana08</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/styles.css">"
+</head>
+<body class="grid-container">
+        <header class="header">
+            <img class="logo" src="images/logo.png" alt="Logo">
+        </header>
+        <nav class="navbar">
+            <ul>
+                <li>Principal</li>
+                <li>Usuarios</li>
+                <li>Empleados</li>
+                <li>Proyectos</li>
+                <li>Reportes</li>
+            </ul>
+        </nav>
+        <section class="main"></section>
+        <footer class="footer">Derechos Grupo 3</footer>
+        <script src="scrips/main.js" type="text/javascript"></script>
+    </body>
+</html>
